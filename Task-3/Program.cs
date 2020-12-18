f7d8a9e3420991300da35d17919e1b8c7ca7adac
vlad <zubrov@list.ru>
2020-12-18 15:13:39 +0300

    3-2

--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -10,16 +10,30 @@ namespace Task_3
     {
         static void Main()
         {
-            Console.Write("Введите слово  ");
-            string a = Console.ReadLine();
-            string b="";
-                        
-            for (int i = a.Length; i>0; i--)
-            {
-                b = b + a[i-1]; 
-            }
-            Console.Write(b);
+            string[,] a = new string[5, 2];
+            int i = 0;
+            int j = 0;
+            a[i, j++] = "Том";
+            a[i++, j--] = "1-2-3/123@ya.ru";
+
+            a[i, j++] = "Джим";
+            a[i++, j--] = "4-5-6/456@ya.ru";
+
+            a[i, j++] = "Сэм";
+            a[i++, j--] = "7-8-9/789@ya.ru";
+
+            a[i, j++] = "Боб";
+            a[i++, j--] = "11-22/1122@ya.ru";
+
+            a[i, j++] = "Джоб";
+            a[i, j] = "33-44/3344@ya.ru";
+
+            Console.WriteLine("Введите имя");
+            string Name = Console.ReadLine();
+            for (int g=0; g< a.GetLength(0); g++) { if (Name == a[g, 0]) { Console.WriteLine($"{a[g, 1]}"); } }
             Console.ReadLine();
+
+
         }
     }
 }
