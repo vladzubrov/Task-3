4639a421cd3f4f16ae99a4ff088f7c988c24a572
vlad <zubrov@list.ru>
2020-12-18 11:18:43 +0300

    Добавьте файлы проекта.

---
+++ Task-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите слово  ");
+            string a = Console.ReadLine();
+            string b="";
+                        
+            for (int i = a.Length; i>0; i--)
+            {
+                b = b + a[i-1]; 
+            }
+            Console.Write(b);
+            Console.ReadLine();
+        }
+    }
+}
